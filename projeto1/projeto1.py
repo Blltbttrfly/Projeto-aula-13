9971cd0c84e7d3d6036831bd2fbb7717bd88fc15
LayLeo <layusais23+tds23@gmail.com>
2026-04-28 21:21:33 -0300

    temperatura

---
+++ projeto1/projeto1.py
@@ -0,0 +1,33 @@
+temperatura = None
+soma_temps = 0
+contador_temperaturas = 0
+media = 0
+maior = None
+menor = None
+vezes = int(input("Digite quantas vezes voce quer checar sua temperatura: "))
+
+
+
+for i in range(vezes):
+    temperatura = float(input(f"Digite a temperatura {i+1}: "))
+    soma_temps += temperatura
+    contador_temperaturas += 1 
+
+    if maior == None:
+        maior = temperatura
+    
+    if temperatura > maior:
+        maior = temperatura
+    
+    if menor == None:
+            menor = temperatura
+    if temperatura < menor:
+            menor = temperatura
+    
+
+media = soma_temps / contador_temperaturas
+
+print(f"essa é sua media bleh: {media}")
+print(f"A maior temperatura é {maior}")
+print(f"A menor temperatura é {menor}")
+
